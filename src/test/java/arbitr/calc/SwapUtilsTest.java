7ff07f24f7ad6b826df94ff0dc59564680856ba6
alexandr <k4t4kun@gmail.com>
2023-07-03 11:06:42 +0200

    change Fee in SwapUtilsTest

--- src/test/java/arbitr/calc/SwapUtilsTest.java
+++ src/test/java/arbitr/calc/SwapUtilsTest.java
@@ -30,7 +30,7 @@ public class SwapUtilsTest {
 
         assertEquals("KCS", swaps[0].getFromCoin());
         assertEquals("DOGE", swaps[0].getToCoin());
-        assertEquals(new BigDecimal("0.001"), swaps[0].getFee());
+        assertEquals(new BigDecimal("0.003"), swaps[0].getFee());
         assertFalse(swaps[0].isReversNeed());
         assertEquals(OrderType.ASK, swaps[0].getOrderType());
         assertEquals("DOGE-KCS", swaps[0].getTicker());
@@ -38,7 +38,7 @@ public class SwapUtilsTest {
 
         assertEquals("DOGE", swaps[1].getFromCoin());
         assertEquals("BTC", swaps[1].getToCoin());
-        assertEquals(new BigDecimal("0.001"), swaps[1].getFee());
+        assertEquals(new BigDecimal("0.003"), swaps[1].getFee());
         assertTrue(swaps[1].isReversNeed());
         assertEquals(OrderType.BID, swaps[1].getOrderType());
         assertEquals("DOGE-BTC", swaps[1].getTicker());
@@ -46,7 +46,7 @@ public class SwapUtilsTest {
 
         assertEquals("BTC", swaps[2].getFromCoin());
         assertEquals("KCS", swaps[2].getToCoin());
-        assertEquals(new BigDecimal("0.001"), swaps[2].getFee());
+        assertEquals(new BigDecimal("0.003"), swaps[2].getFee());
         assertFalse(swaps[2].isReversNeed());
         assertEquals(OrderType.ASK, swaps[2].getOrderType());
         assertEquals("KCS-BTC", swaps[2].getTicker());
